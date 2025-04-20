de06a2021fbd726d6b95f94a5098e2e3aad04794
hichamDe <hicham.iamiri200@gmail.com>
2025-04-20 17:48:30 +0100

    fixed build error

---
+++ car-price-predictor/src/app/components/Footer.js
@@ -0,0 +1,19 @@
+import { motion } from 'framer-motion';
+
+const Footer = () => {
+    return (
+        <>
+            {/* Footer */}
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1, duration: 0.5 }}
+                className="mt-8 text-center text-gray-500 text-sm"
+            >
+                © {new Date().getFullYear()} Moroccan Car Price Predictor | All rights reserved
+            </motion.div>
+        </>
+    );
+}
+
+export default Footer;
---
+++ car-price-predictor/src/app/data/config.js
@@ -0,0 +1,11 @@
+export const data = {
+    carBrands: [
+        'Dacia', 'Renault', 'Peugeot', 'Volkswagen', 'Toyota', 'Ford',
+        'Mercedes', 'BMW', 'Audi', 'Hyundai', 'Kia', 'Fiat', 'Seat',
+        'Nissan', 'Citroen'
+    ],
+    carConditions : ['excellent', 'good', 'fair', 'poor'],
+    origin:[],
+    fuelType:['Diesel','Gasoline','Hybrid','Electric']
+
+}
