eb53cf40662536ecf284fc22ec2c2b9daeef785e
hichamDe <hicham.iamiri200@gmail.com>
2025-04-20 17:57:20 +0100

    next.config.mjs

--- car-price-predictor/next.config.mjs
+++ car-price-predictor/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    rewrites: async () => [
+        { source: '/', destination: '/nonexistent' } // -> will 404
+    ],
+};
 
 export default nextConfig;
